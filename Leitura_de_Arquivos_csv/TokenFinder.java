e4ed4dd0f0df3d1a40f02b1c23c31a7925d1d69f
Nathália Yukimi <129632321+senshiacy@users.noreply.github.com>
2024-07-22 20:36:44 -0300

    Update TokenFinder.java

--- Leitura_de_Arquivos_csv/TokenFinder.java
+++ Leitura_de_Arquivos_csv/TokenFinder.java
@@ -1,22 +1,3 @@
-/******************************************************************************
- *
- * MAC0121 ALGORITMOS E ESTRUTURAS DE DADOS I
- * Aluno: Nathália Yukimi Uchiyama Tsuno
- * Numero USP: 14600541
- * Tarefa: E05
- * Data: 03 de Dezembro de 2023
- * 
- * Baseado em ... (breve e se aplicável)
- *
- * DECLARO QUE SOU O ÚNICO AUTOR E RESPONSÁVEL POR ESTE PROGRAMA.  TODAS AS 
- * PARTES DO PROGRAMA, EXCETO AS QUE SÃO BASEADAS EM MATERIAL FORNECIDO  
- * PELO PROFESSOR OU COPIADAS DO LIVRO OU DAS BIBLIOTECAS DE SEDGEWICK & WAYNE, 
- * FORAM DESENVOLVIDAS POR MIM.  DECLARO TAMBÉM QUE SOU RESPONSÁVEL POR TODAS 
- * AS CÓPIAS DESTE PROGRAMA E QUE NÃO DISTRIBUÍ NEM FACILITEI A DISTRIBUIÇÃO
- * DE CÓPIAS DESTA PROGRAMA.
- *
- ******************************************************************************/
-
 public class TokenFinder{
 	// Variáveis de instância
 	private final String line;
